fba490e4688c03f3a0beaea61d6c40655c0d8fc5
mimihwa <wmfskf@naver.com>
2023-02-10 11:02:09 +0900

    230210 1st finished

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,37 +3,54 @@ import './MainPage.css'
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 import dayjs from 'dayjs';
+import { Carousel } from 'antd';
+import {API_URL} from '../config/constants'
+
 import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
 
 
 const MainPage = () => {
     let [products, setProducts] = useState([]);
-
+    const [banners, setBanners] =useState([]);
     useEffect(() => {
-        axios.get('http://localhost:8080/products/')
-        .then((result) => {
+        axios.get(`${API_URL}/products/`).then((result) => {
             products = result.data.product;
             setProducts(products);
         }).catch((error) => {
             console.log(`통신실패 : ${error}`)
         });
+        axios.get(`${API_URL}/banners/`).then((result)=>{
+            const banners=result.data.banners;
+            setBanners(banners);
+        }).catch((error)=>{
+            console.log(`통신실패 : ${error}`)
+        })
     }, []);
 
     return (
         <div>
-            <div id="banner">
-                <img src="./images/banners/banner1.png" alt="mainImg" />
-            </div>
+            <Carousel autoplay autoplaySpeed={2000}>
+                {banners.map((banner, index)=>{
+                    return(
+                        <Link to={banner.href} key={index}>
+                            <div id="banner">
+                                <img src={`${API_URL}/${banner.imageUrl}`} alt="mainImg" />
+                            </div>
+                        </Link>
+                    )
+                })}
+            </Carousel>
             <h1>Products</h1>
             <div id="product-list">
                 {products.map((product, idx) => {
                     // console.log(product);
                     return (
                     <div className="product-card" key={idx}>
+                        {product.soldout === 1 ? <div className="product-blur"></div> : null}
                         <Link className='product-link' to={`/ProductPage/${product.id}`}>
                             <div>
-                                <img src={product.imageUrl} alt={product.name} className="product-img" />
+                                <img src={`${API_URL}/${product.imageUrl}`} alt={product.name} className="product-img" />
                             </div>
                             <div className="product-contents">
                                 <span className="product-name">{product.name}</span>
--- src/components/UploadPage.js
+++ src/components/UploadPage.js
@@ -1,68 +1,79 @@
-import React, { useState } from 'react';
-import { Button, Upload, message, InputNumber, Checkbox, Form, Input, Divider } from 'antd';
-import "./UploadPage.scss"
-import {useNavigate} from "react-router-dom"
-import axios from 'axios';
+import React, {useState} from 'react';
+import { Button, Upload, message, InputNumber, Form, Input, Divider } from 'antd';
+import "./UploadPage.scss";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import {API_URL} from '../config/constants'
 
-const UploadPage = () => {
-    /* const [imageUrl, setImageUrl] = useState(null); */
-    const history = useNavigate();
-    const onsubmit = (values) => {
-        axios.post(``,{
+
+
+export default function UploadPage() {
+    const [imageUrl, setImageUrl] = useState(null); 
+    const history=useNavigate();
+    const onSubmit = (values) => {
+        axios.post(`${API_URL}/products`,{
             name: values.name,
             description: values.description,
             seller: values.seller,
-            price: values.price,
-        })
-        .then((result)=>{
+            price: parseInt(values.price),
+            imageUrl: imageUrl , 
+        }).then((result) => {
             console.log(result);
             history('/', {replace:true});
-        }).catch((error)=>{
+        }).catch((error) =>{
             console.log(error);
-            message.error(`에러가 발생했습니다 $(error.message)`)
+            message.error(`에러가 발생했습니다 ${error.message}`);
         });
+
     };
-    const onChangeImage = (info)=>{
-        /* if(){
-            return
+    const onChangeImage = (info) => {
+        if(info.file.status==='uploading'){
+            return;
         }
-        if(){} */
+        if(info.file.status==="done"){
+           const response=info.file.response;
+           const imageUrl=response.imageUrl;
+
+           setImageUrl(imageUrl);
+        } 
     }
-        
-    return (
-        <div id='upload-container'>
-            <Form name='uploadForm' onFinish={onsubmit}>
-                <Form.Item name="upload" label={<span className='upload-label'>상품사진</span>}>
-                    <div id='upload-img' onChange={onChangeImage}>
-                        <img src="/images/icons/camera.png" alt="" />
-                        <span>이미지를 업로드 해주세요</span>
-                    </div >
-                </Form.Item>
-                <Divider/>
-                <Form.Item label={<span className='upload-label'>판매자명</span>} name="seller" rules={[ {required: true, message: '판매자명은 필수 입력사항입니다.'} ]} >
-                    <Input className='upload-name' placeholder='이름을 입력해주세요!' size='large'/>
-                </Form.Item>
-                <Divider/>
-                <Form.Item label={<span className='upload-label'>상품명</span>} name="name" rules={[ {required: true, message: '상품명은 필수 입력사항입니다.'} ]} >
-                    <Input className='upload-name' placeholder='상품명을 입력해주세요!' size='large'/>
-                </Form.Item>
-                <Divider/>
-                <Form.Item label={<span className='upload-label'>판매가</span>} name="price" rules={[ {required: true, message: '판매가는 필수 입력사항입니다.'} ]} >
-                    <InputNumber className='upload-price' min={0} size='large'/>
-                </Form.Item>
-                <Divider/>
-                <Form.Item label={<span className='upload-label'>상품설명</span>} name="description" rules={[ {required: true, message: '상품설명을 해주세요.'} ]} >
-                    <Input.TextArea placeholder='상품설명을 해주세요!' size='large' id='product-description' showCount maxLength={300}/>
-                </Form.Item>
-                <Divider/>
-                <Form.Item>
-                    <Button id='submit-button' size='large' htmlType='submit'>상품등록하기</Button>
-                </Form.Item>
-            </Form>
-        </div> 
-    );
-};
 
-
-
-export default UploadPage;
+  return (
+    <div id="upload-container">
+        <Form name="uploadFrom" onFinish={onSubmit} initialValues={{price:0}}>
+            <Form.Item name="upload" label={<div className='upload-label'>상품 사진</div>}>
+                <Upload name="image" action={`${API_URL}/image`} listType='picture' showUploadList={false} onChange={onChangeImage}>
+                    { imageUrl ? (<img id="upload-img" src={`${API_URL}/${imageUrl}` } alt="이미지 왜 안나와" />
+                    ):(
+                        <div id="upload-img" >
+                            <img src="/images/icons/camera.png" alt="" />
+                            <span>이미지를 업로드 해주세요</span>
+                        </div>
+                    )}
+                </Upload>
+            </Form.Item>
+            <Divider />
+            <Form.Item label={<span className='upload-label'>판매자명</span>}  name="seller" rules={[ { required: true, message: '판매자명을 입력해주세요' }]}>
+                <Input  className='upload-name' placeholder='이름을 입력해주세요' size="large" />
+            </Form.Item>
+            <Divider />
+            <Form.Item label={<span className='upload-label'>상품명</span>}  name="name" rules={[ { required: true, message: '상품명은 필수 입력사항입니다' }]}>
+                <Input  className='upload-name' placeholder='상품명을 입력해주세요' size="large" />
+            </Form.Item>
+            <Divider />
+            <Form.Item label={<span className='upload-label'>판매가</span>}  name="price" rules={[ { required: true, message: '판매가를 입력해주세요' }]}>
+                <InputNumber  className='upload-price' min={0} size="large" />
+            </Form.Item>
+            <Divider />
+            <Form.Item label={<span className='upload-label'>상품설명</span>}  name="description" rules={[ { required: true, message: '상품설명을 입력해주세요' }]}>
+                <Input.TextArea  size="large" id="product-description" showCount maxLength={300} placeholder="상품설명을 작생해주세요" />
+            </Form.Item>
+            <Divider />
+            <Form.Item>
+                <Button id="submit-button" size='large' htmlType='submit'>상품등록하기</Button>
+            </Form.Item>
+            
+        </Form>
+    </div>
+  )
+}
